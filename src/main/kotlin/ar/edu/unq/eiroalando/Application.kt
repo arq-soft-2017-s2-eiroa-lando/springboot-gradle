b681d1149dd8ff5ae7dc1de97d354b2c89bcf6dc
eiroa <eiroam@gmail.com>
2017-10-27 22:28:13 -0300

    Fixed building script

--- src/main/kotlin/ar/edu/unq/eiroalando/Application.kt
+++ src/main/kotlin/ar/edu/unq/eiroalando/Application.kt
@@ -61,5 +61,5 @@ open class WebConfig : WebMvcConfigurerAdapter() {
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java)
+    SpringApplication.run(Application::class.java, *args)
 }
